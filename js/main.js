9d9062d8646d0a2b059cc0b89f3f1ad73ab9bf44
Sneha Khatun <reactdevsneha143@gmail.com>
2024-11-02 09:27:02 +0530

    urgroomer image and content added

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ navBar.forEach(function(a){
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Purrfectly Groomed"];
+const textArray = ["Urgroomer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -85,4 +85,160 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
   e.preventDefault();
   document.querySelector('.subscription').classList.add('done');
+});
+// particle js 
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": ["#ffffff","#f1c40f"]
+      },
+      "shape": {
+        "type": ["circle","triangle"],
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 7,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+// counter 
+document.addEventListener("DOMContentLoaded", function () {
+  const stats = document.querySelectorAll('.stat-count');
+
+  stats.forEach(stat => {
+      const updateStat = () => {
+          const target = +stat.getAttribute('data-target');
+          const count = +stat.innerText;
+          const speed = 200;
+          const increment = target / speed;
+
+          if (count < target) {
+              stat.innerText = Math.ceil(count + increment);
+              setTimeout(updateStat, 1);
+          } else {
+              stat.innerText = target;
+          }
+      };
+
+      updateStat();
+  });
 });
